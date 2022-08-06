0d1fd36764a617a7f3a5f13bacaa6aaf67893042
John Desrosiers <john.desro@gmail.com>
2022-08-06 19:34:15 +0000

    Fix warning message in App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,16 @@
 import App from "./App";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 
 test("should render LazyComponent", async () => {
   render(<App />);
-
+  
   // assert that loader renders
-  await screen.findByText(/loading/i);
-
+  // Wrap in act to avoid the warning about wrapping in an act call.
+  await act(() => screen.findByText(/loading/i))
+  
   // assert that lazy component renders
   await screen.findByText(/lazy component/i);
-
+  
   // assert that loader was removed
   expect(screen.queryByText(/loading/i)).toBeNull();
 });
